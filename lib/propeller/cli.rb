3bf95b1b9d4aa4810a23c9434326c75429e86d4e
wilkie <wilkie05@gmail.com>
2012-10-18 20:37:40 -0400

    Adds CLI class for command line usage.

---
+++ lib/propeller/cli.rb
@@ -0,0 +1,72 @@
+require 'optparse'
+
+require 'propeller'
+require 'propeller/configuration'
+
+module Propeller
+  class CLI
+    BANNER = <<-USAGE
+    Usage:
+      gruesome play STORY_FILE
+
+    Description:
+      The 'play' command will start a session of the story given as STORY_FILE
+
+    Example:
+      gruesome play zork1.z3
+
+    USAGE
+
+    class << self
+      def parse_options
+        @opts = OptionParser.new do |opts|
+          opts.banner = BANNER.gsub(/^    /, '')
+
+          opts.separator ''
+          opts.separator 'Options:'
+
+          opts.on('-h', '--help', 'Display this help') do
+            puts opts
+            exit
+          end
+        end
+
+        @opts.parse!
+      end
+
+      def CLI.run
+        begin
+          parse_options
+        rescue OptionParser::InvalidOption => e
+          warn e
+          exit -1
+        end
+
+        def fail
+          puts @opts
+          exit -1
+        end
+
+        case ARGV.first
+        when 'play'
+          fail unless ARGV[1]
+
+          Gruesome::Logo.display
+
+          puts
+          puts "--------------------------------------------------------------------------------"
+          puts
+
+          Gruesome::Machine.new(ARGV[1]).execute
+        else
+          config = Propeller::Configuration.new
+          config.addons.each do |addon|
+            puts "Would you like to add #{addon.name} support?"
+            puts "#{addon.description}"
+            puts "(y/N): "
+          end
+        end
+      end
+    end
+  end
+end
