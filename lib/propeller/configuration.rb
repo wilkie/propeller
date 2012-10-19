63da4d22b5d3384d86d1178a414127e61f6b6f82
wilkie <wilkie05@gmail.com>
2012-10-18 20:37:26 -0400

    Adds Configuration class.

---
+++ lib/propeller/configuration.rb
@@ -0,0 +1,29 @@
+require 'yaml'
+
+require 'propeller/addon'
+
+module Propeller
+  class Configuration
+    attr_accessor :addons
+
+    def initialize(options = {})
+      options = {:config_file => "config/blade.yml"}.merge(options)
+
+      @yaml = load_yaml(options[:config_file])
+
+      @addons = @yaml['addons'].map do |addon|
+        addon.keys.each do |key|
+          addon[(key.to_sym rescue key) || key] = addon.delete(key)
+        end
+
+        Propeller::Addon.new addon
+      end
+    end
+
+    private
+
+    def load_yaml(config)
+      YAML::load_file(config)
+    end
+  end
+end
