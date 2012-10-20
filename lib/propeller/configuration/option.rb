4ef6aea0dbec70ae296445b9916b81f5d791a4ae
wilkie <wilkie05@gmail.com>
2012-10-20 14:27:21 -0400

    Adds min and max to Option.

--- lib/propeller/configuration/option.rb
+++ lib/propeller/configuration/option.rb
@@ -7,12 +7,16 @@ module Propeller
       attr_accessor :description
       attr_accessor :type
       attr_accessor :default
+      attr_accessor :min
+      attr_accessor :max
 
       def initialize(option_hash)
         @name        = option_hash[:name]
         @description = option_hash[:description]
         @type        = option_hash[:type]
         @if          = option_hash[:if]
+        @min         = option_hash[:min]
+        @max         = option_hash[:max]
         @default     = option_hash[:default] || case @type
                                                 when :bool
                                                   false
