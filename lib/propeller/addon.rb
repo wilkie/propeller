4fa7f491ef18d74ae011e7a971a8c5e7026e2325
wilkie <wilkie05@gmail.com>
2012-10-22 19:31:34 -0400

    Adds downloading of addons and gather addon blades.

--- lib/propeller/addon.rb
+++ lib/propeller/addon.rb
@@ -1,5 +1,9 @@
 module Propeller
   class Addon
+    require 'git'
+    require 'uri'
+    require 'fileutils'
+
     attr_accessor :name
     attr_accessor :git
     attr_accessor :description
@@ -8,6 +12,37 @@ module Propeller
       @name        = addon_hash[:name]
       @git         = addon_hash[:git]
       @description = addon_hash[:description]
+
+      @downloaded = false
+    end
+
+    def blade
+      download
+
+      @blade ||= Propeller::Blade.new(:config_file => ".addons/#{@name}/config/blade.yml",
+                                      :to_env      => false)
+    end
+
+    private
+
+    def download
+      return if @downloaded
+
+      @git.each do |path|
+        begin
+          # TODO : detect that it has been downloaded and pull latest
+          g = Git.clone(URI(path), ".addons/#{@name}")
+          @downloaded = true
+
+          break
+        rescue Git::GitExecuteError => e
+          puts "Error retrieving repository... trying to find another mirror."
+        end
+      end
+    end
+
+    def install
+      download
     end
   end
 end
