6f19921b170609ba9d2fb1a4ac481db71b30f3a1
wilkie <wilkie05@gmail.com>
2012-10-31 03:09:36 -0400

    Adds require functions for addons to interface with main files.
    This is to be discouraged for the vast majority of cases, but is useful
    for testing infrastructure and helper methods that are unlikely to
    change.

--- lib/propeller.rb
+++ lib/propeller.rb
@@ -2,4 +2,17 @@ require "propeller/version"
 require "propeller/addon_manager"
 
 module Propeller
+  def self.configure(path)
+    @@path = path
+  end
+
+  def self.require_from_main(path)
+    basepath = @@path || "."
+    path = "#{basepath}/#{path}"
+  end
+
+  def self.require_from_addon(addon_name, path)
+    basepath = Gem::Specification::find_by_name(addon_name.to_s).gem_dir
+    path = "#{basepath}/#{path}"
+  end
 end
