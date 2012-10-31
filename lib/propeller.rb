cb0e73b7802abfe8805705733e934f8ff1975508
wilkie <wilkie05@gmail.com>
2012-10-31 17:43:20 -0400

    Adds rake files function and path and require functions.

--- lib/propeller.rb
+++ lib/propeller.rb
@@ -6,13 +6,37 @@ module Propeller
     @@path = options[:path]
   end
 
-  def self.require_from_main(path)
+  def self.path_for_main
     @@path ||= "."
-    path = "#{@@path}/#{path}"
   end
 
-  def self.require_from_addon(addon_name, path)
-    basepath = Gem::Specification::find_by_name(addon_name.to_s).gem_dir
-    path = "#{basepath}/#{path}"
+  def self.require_from_main(path)
+    require "#{Propeller.path_for_main}/#{path}"
   end
+
+  def self.path_for(addon_name)
+    Gem::Specification::find_by_name(addon_name.to_s).gem_dir
+  end
+
+  def self.require_from(addon_name, path)
+    require "#{Propeller.path_for(addon_name)}/#{path}"
+  end
+
+  def self.rake_files(query)
+    addon_tests = []
+    blade = Propeller::Blade.new(:to_env => false)
+    blade.selection.addons.each do |addon|
+      search = File.join(Propeller.path_for(addon), query)
+      addon_tests.concat(Dir.glob(search))
+    end
+    addon_tests
+  end
+end
+
+def require_from_main(path)
+  Propeller.require_from_main path
+end
+
+def require_from(addon_name, path)
+  Propeller.require_from addon_name, path
 end
